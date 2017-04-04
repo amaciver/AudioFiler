86bc6a284572da73bddc5fd6ebcd3f43047f15b8
Alec Johnson <alecjohnson55@gmail.com>
2017-04-03 19:10:05 -0700

    initial commit

---
+++ initialize_database.py
@@ -0,0 +1,205 @@
+import sqlite3
+import os
+import json
+import requests
+
+# map common variations of genres to our consistent version
+ACCEPTED_GENRES = {
+    'Alternative': 'Alternative',
+    'Alternative Rock': 'Alternative',
+    'Alternative rock': 'Alternative',
+    'Alt Rock': 'Alternative',
+    'Alt rock': 'Alternative',
+    'Alt. Rock': 'Alternative',
+    'Alt. rock': 'Alternative',
+    'alternative': 'Alternative',
+    'alternative rock': 'Alternative',
+    'alt rock': 'Alternative',
+    'alt. rock': 'Alternative',
+    'Blues': 'Blues',
+    'blues': 'Blues',
+    'blues rock': 'Blues',
+    'Classical': 'Classical',
+    'classical': 'Classical',
+    'Alt-country': 'Country',
+    'Country': 'Country',
+    'country': 'Country',
+    'D&B': 'D&B',
+    'D&b': 'D&B',
+    'Drum And Bass': 'D&B',
+    'Drum and Bass': 'D&B',
+    'Drum and bass': 'D&B',
+    'd&b': 'D&B',
+    'drum and bass': 'D&B',
+    'Disco': 'Disco',
+    'disco': 'Disco',
+    'Dubstep': 'Dubstep',
+    'dubstep': 'Dubstep',
+    'Electronic': 'Electronic',
+    'Electronica': 'Electronic',
+    'electronic': 'Electronic',
+    'electronica': 'Electronic',
+    'electro': 'Electronic',
+    'Techno': 'Electronic',
+    'techno': 'Electronic',
+    'Folk': 'Folk',
+    'folk': 'Folk',
+    'Funk': 'Funk',
+    'funk': 'Funk',
+    'Hip-Hop': 'Hip-Hop',
+    'Hip/Hop': 'Hip-Hop',
+    'Hip Hop': 'Hip-Hop',
+    'HipHop': 'Hip-Hop',
+    'Hip-hop': 'Hip-Hop',
+    'Hip/hop': 'Hip-Hop',
+    'Hip hop': 'Hip-Hop',
+    'Hiphop': 'Hip-Hop',
+    'hip-hop': 'Hip-Hop',
+    'hip/hop': 'Hip-Hop',
+    'hip hop': 'Hip-Hop',
+    'hiphop': 'Hip-Hop',
+    'House': 'House',
+    'EDM/House': 'House',
+    'EDM': 'House',
+    'house': 'House',
+    'deep house': 'House',
+    'electro house': 'House',
+    'tech house': 'House',
+    'Indie': 'Indie',
+    'Independent': 'Indie',
+    'Indie Rock': 'Indie',
+    'Indie rock': 'Indie',
+    'indie': 'Indie',
+    'indie pop': 'Indie',
+    'independent': 'Indie',
+    'indie rock': 'Indie',
+    'Jazz': 'Jazz',
+    'Smooth Jazz': 'Jazz',
+    'jazz': 'Jazz',
+    'Metal': 'Metal',
+    'Death-Metal': 'Metal',
+    'Death Metal': 'Metal',
+    'Death-metal': 'Metal',
+    'Death metal': 'Metal',
+    'Heavy Metal': 'Metal',
+    'Heavy metal': 'Metal',
+    'heavy metal': 'Metal',
+    'Power Metal': 'Metal',
+    'Power metal': 'Metal',
+    'power metal': 'Metal',
+    'Thrash Metal': 'Metal',
+    'Thrash metal': 'Metal',
+    'thrash metal': 'Metal',
+    'doom metal': 'Metal',
+    'Progressive Metal': 'Metal',
+    'Progressive metal': 'Metal',
+    'progressive metal': 'Metal',
+    'metal': 'Metal',
+    'death metal': 'Metal',
+    'death-metal': 'Metal',
+    'black metal': 'Metal',
+    'Pop': 'Pop',
+    'pop': 'Pop',
+    'Punk': 'Punk',
+    'Punk Rock': 'Punk',
+    'Punk rock': 'Punk',
+    'punk': 'punk',
+    'punk rock': 'Punk',
+    'hardcore punk': 'Punk',
+    'post-punk': 'Punk',
+    'RB': 'R&B',
+    'R&B': 'R&B',
+    'RandB': 'R&B',
+    'R And B': 'R&B',
+    'R and B': 'R&B',
+    'R and b': 'R&B',
+    'rhythm and blues': 'R&B',
+    'r&b': 'R&B',
+    'rnb': 'R&B',
+    'randb': 'R&B',
+    'r and b': 'R&B',
+    'Rap': 'Rap',
+    'rap': 'Rap',
+    'Gangsta Rap': 'Rap',
+    'Rap/Hip-Hop': 'Rap',
+    'Rap/Hip Hop': 'Rap',
+    'Rap/Hip-hop': 'Rap',
+    'Rap/Hip hop': 'Rap',
+    'rap/hip-hop': 'Rap',
+    'rap/hip hop': 'Rap',
+    'Reggaeton': 'Reggaeton',
+    'reggaeton': 'Reggaeton',
+    'Reggae': 'Reggae',
+    'reggae': 'Reggae',
+    'Rock': 'Rock',
+    'Rock and Roll': 'Rock',
+    'rock n roll': 'Rock',
+    'rock': 'Rock',
+    'Hard Rock': 'Rock',
+    'Hard rock': 'Rock',
+    'hard rock': 'Rock',
+    'Salsa': 'Salsa',
+    'salsa': 'Salsa',
+    'Ska': 'Ska',
+    'ska': 'Ska',
+    'Soul': 'Soul',
+    'soul': 'Soul',
+    'Hard Trance': 'Trance',
+    'Trance': 'Trance',
+    'trance': 'Trance',
+    'psytrance': 'Trance',
+    }
+
+# create new database
+conn = sqlite3.connect('subset.db')
+c = conn.cursor()
+
+c.execute('''DROP TABLE IF EXISTS tracks''')
+c.execute('''CREATE TABLE tracks (id INTEGER PRIMARY KEY, title TEXT, artist TEXT, genre TEXT, preview_url TEXT)''')
+# iterate through the directory, check the top 3 tags, and use the first one that is in our list as the genre.
+# if it has none, skip it. Otherwise, send a query to spotify with the title and artist, and retreive the preview URL.
+# if the query to spotify comes back empty, continue to the next one.
+for root, directory, files in os.walk('./subset'):
+    for file in files:
+        # take care of .ds_store
+        if file.startswith('.'):
+            continue
+        with open(root + '/' + file) as data_file:
+            data = json.load(data_file)
+            track_title = data['title']
+            track_artist = data['artist']
+            genre1 = None
+            genre2 = None
+            genre3 = None
+
+            try:
+                genre1 = data['tags'][0][0]
+                genre2 = data['tags'][1][0]
+                genre3 = data['tags'][2][0]
+            except IndexError:
+                pass
+
+            make_query = True
+
+            if genre1 in ACCEPTED_GENRES:
+                genre = genre1
+            elif genre2 in ACCEPTED_GENRES:
+                genre = genre2
+            elif genre3 in ACCEPTED_GENRES:
+                genre = genre3
+            else:
+                make_query = False
+
+            if make_query:
+                base_url = "https://api.spotify.com/v1/search?q="
+                title = f"track:\"{data['title']}\""
+                artist = f"artist:\"{data['artist']}\""
+                response = requests.get(base_url + title + "%20" + artist + "&type=track&limit=1")
+                try:
+                    preview_url = response.json()['tracks']['items'][0]['preview_url']
+                    track = (track_title, track_artist, ACCEPTED_GENRES[genre], preview_url)
+                    c.execute('''INSERT INTO tracks(title, artist, genre, preview_url) VALUES(?,?,?,?)''', track)
+                except IndexError:
+                    pass
+conn.commit()
+conn.close()
