12b17d1b80d0619354c9a6c26a0987b79bc35cde
Andrew MacIver <Andrew@Andrews-MacBook-Air-4.local>
2017-04-05 09:15:04 -0700

    clean out nist tutorials

--- model.py
+++ model.py
@@ -120,5 +120,10 @@ for _ in range(1000):
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+
+# correct = tf.nn.in_top_k(logits, labels, 1)
+# # Return the number of true entries.
+# return tf.reduce_sum(tf.cast(correct, tf.int32))
+
 print(sess.run(accuracy, feed_dict={x: na,
                                     y_: labels}))
