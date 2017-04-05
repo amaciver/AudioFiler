9375464e394c15e9bd6f637727ee9887a8e9abdb
Alec Johnson <alecjohnson55@gmail.com>
2017-04-05 11:57:42 -0700

    add comment explaining script

--- database_modification/directory_walk.py
+++ database_modification/directory_walk.py
@@ -2,6 +2,8 @@ import os
 import json
 import re
 
+# by padding filenames we can ensure that the feature extraction and
+# other scripts iterate over them in a consistent and linear order
 def pad(num):
     while len(num) < 4:
         num = '0' + num
