f5d99bc6410a92913ac3d7376a1f33d51136c61f
Adom Hartell <adomhartell@gmail.com>
2017-04-05 17:20:50 -0700

    write skeleton for eventual show/analysis view

--- analysis_backend/analysis/views.py
+++ analysis_backend/analysis/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def show(spotifyId):
+    # get mp3 from spotify
+
+    # perform analysis
+
+    # format json response 
+    return response
