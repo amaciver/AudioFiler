e8c1f463bc6b9c581e62c0759b6c0a5ccba093a8
Adom Hartell <adomhartell@gmail.com>
2017-04-05 20:34:25 -0700

    add analysis code to util.py

--- analysis_backend/helpers/util.py
+++ analysis_backend/helpers/util.py
@@ -1,4 +1,9 @@
 import requests
+# import numpy as np
+
+# from analysis_backend.helpers.pyAudioAnalysis.audioFeatureExtraction import *
+# from analysis_backend.helpers.pyAudioAnalysis import audioAnalysis as aA
+# from analysis_backend.helpers.pyAudioAnalysis import audioBasicIO as audioBasicIO
 
 def downloadPreview(url):
     # r = requests.get(preview_url)
@@ -8,3 +13,51 @@ def downloadPreview(url):
 
 
 downloadPreview("https://p.scdn.co/mp3-preview/7c548326fe87f76161403d00bbb3b65b988a2c17?cid=null")
+
+#
+# def bpm(filename):
+#     Fs, x = audioBasicIO.readAudioFile(filename)
+#     x_ = audioBasicIO.stereo2mono(x)
+#     F = aF.stFeatureExtraction(x_, Fs, 0.050 * Fs, 0.050 * Fs)
+#     BPM, ratio = aF.beatExtraction(F, 0.050, False)
+#     return BPM
+#
+# vectors, filenames = aF.dirWavFeatureExtraction('./data/', 60, 30, 1, 1)
+# bpms = map(bpm, [filenames])
+#
+# vectors_bpms = zip([vectors], bpms)
+#
+# arr = []
+# for entry in vectors_bpms:
+#     joined = np.append(entry[0], entry[1])
+#     joined = joined.tolist()
+#     arr.append(joined)
+#
+# arr = np.array(arr)
+# results = zip(filenames, arr)
+#
+# arr2 = []
+# for entry in results:
+#     arr2.append(entry[1])
+#
+# # print results
+# # print arr2
+#
+# ##read/write csv file
+# # import csv
+# # with open("test.csv", "wb") as the_file:
+# #     csv.register_dialect("custom", delimiter=" ", skipinitialspace=True)
+# #     writer = csv.writer(the_file, dialect="custom")
+# #     for tup in results:
+# #         writer.writerow(tup)
+# #
+# # reader = csv.reader(open("test.csv", "rb"), delimiter=' ')
+# # for row in reader:
+# #     print row
+#
+# print arr2
+#
+# ## save np object to txt
+# # np.savetxt("subset_1000.txt", arr2)
+# # load = np.loadtxt("test.txt")
+# # print (np.array(load)[0])
