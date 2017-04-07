a7c43e528e74d094f198de4713ba780364442fbb
Adom Hartell <adomhartell@gmail.com>
2017-04-07 12:19:50 -0700

    working docker installation

--- dockerAnalysis/analysis_backend/analysis/views.py
+++ dockerAnalysis/analysis_backend/analysis/views.py
@@ -5,7 +5,6 @@ from helpers import util
 
 def show(request, previewUrl):
     util.downloadPreview(previewUrl)
-
     array = util.extractFeatures()
-
+    
     return JsonResponse({ 'features': array })
