bac32b250ff2c18a5e1e6b60038e3b8f4168bf01
Alec Johnson <alecjohnson55@gmail.com>
2017-04-08 23:26:18 -0700

    set both servers to talk to each other

--- docker_backend/model_backend/model/views.py
+++ docker_backend/model_backend/model/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from requests import Session
-session = Session()
+import requests
+
 
 # import classify from util
 
-# from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def show(request, preview_url):
-    response = session.get(
-    url=f"localhost:8001/analysis/{preview_url}")
-    return("we hit the route")
+    url = f"http://localhost:8001/analysis/{preview_url}"
+    response = requests.get(url)
+    print(response)
+    return JsonResponse({"key": response.text})
     # return(classify(response))
