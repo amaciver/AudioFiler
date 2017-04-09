a683970cb14bec770482356f941f96bea914e44f
Alec Johnson <alecjohnson55@gmail.com>
2017-04-08 22:51:57 -0700

    moved folders

--- model_backend/model/static/assets/js/util/results_api_util.js
+++
@@ -1,7 +0,0 @@
-export const fetchResults = (url_string) => {
-  return $.ajax({
-    method: 'GET',
-    url: '',
-    data: {url_string}
-  });
-};
--- model_backend/model/static/assets/js/util/tracks_api_util.js
+++
@@ -1,9 +0,0 @@
-import 'jquery'
-
-export const fetchTracks = (query_string) => {
-
-  return $.ajax({
-    method: 'GET',
-    url: `https://api.spotify.com/v1/search?q=track:${query_string}&type=track&limit=5`
-  });
-};
