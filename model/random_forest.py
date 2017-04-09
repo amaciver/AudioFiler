4654da5217633ac40d145c7975b95855f85be261
Alec Johnson <alecjohnson55@gmail.com>
2017-04-08 21:10:34 -0700

    complete compilation of data from audio analysis, set paths for learning model to local

--- model/random_forest.py
+++ model/random_forest.py
@@ -192,15 +192,44 @@ import pandas as pd
 import numpy as np
 
 # genres = np.loadtxt("genre_tags.txt")
-subset = np.loadtxt("subset_1000.txt")
-genres_nothot = np.loadtxt("genre_tags_not_one_hot.txt")
+# subset = np.loadtxt("subset_1000.txt")
+# genres_nothot = np.loadtxt("genre_tags_not_one_hot.txt")
 
-na_train = np.array(subset[0:800], dtype="float32")
-labels_train_nothot = np.array(genres_nothot[0:800], dtype="int32")
+# na_train = np.array(subset[0:800], dtype="float32")
+# labels_train_nothot = np.array(genres_nothot[0:800], dtype="int32")
+#
+# na_test = np.array(subset[800:], dtype="float32")
+# labels_test_nothot = np.array(genres_nothot[800:], dtype="int32")
 
-na_test = np.array(subset[800:], dtype="float32")
-labels_test_nothot = np.array(genres_nothot[800:], dtype="int32")
 
+genres_nothot = np.loadtxt("/Volumes/Seagate Expansion Drive/AudioFiler/single_num_genre_tags.txt")
+
+# subset = np.loadtxt("subset_1000.txt")
+na_train = np.array(np.loadtxt(f"/Volumes/Seagate Expansion Drive/AudioFiler/batches/batch_1.txt"), dtype="float32")
+na_test = np.array(np.loadtxt(f"/Volumes/Seagate Expansion Drive/AudioFiler/batches/batch_24.txt"), dtype="float32")
+
+for i in range(2, 24):
+    data = np.loadtxt(f"/Volumes/Seagate Expansion Drive/AudioFiler/batches/batch_{i}.txt")
+    na_train = np.concatenate((na_train, data))
+
+for i in range(25, 29):
+    data = np.loadtxt(f"/Volumes/Seagate Expansion Drive/AudioFiler/batches/batch_{i}.txt")
+    na_test = np.concatenate((na_test, data))
+
+labels_train_nothot = np.array(genres_nothot[0:len(na_train)], dtype="int32")
+labels_test_nothot = np.array(genres_nothot[len(na_train):], dtype="int32")
+# na_train = np.array(subset[0:800], dtype="float32")
+# labels_train = np.array(genres[0:800], dtype="float32")
+# labels_train_nothot = np.array(genres_nothot[0:800], dtype="int32")
+
+# na_test = np.array(subset[800:], dtype="float32")
+# labels_test = np.array(genres[800:], dtype="float32")
+# labels_test_nothot = np.array(genres_nothot[800:], dtype="int32")
+
+print (len(na_train))
+print (len(labels_train_nothot))
+print (len(na_test))
+print (len(labels_test_nothot))
 # iris = load_iris()
 # df = pd.DataFrame(na_train)
 # df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
