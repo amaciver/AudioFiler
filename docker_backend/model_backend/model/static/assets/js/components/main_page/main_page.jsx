d9bf49c5073bbb686a025963fe62fd57a7771cb2
Andrew MacIver <maciver.andrew@gmail.com>
2017-04-09 21:47:31 -0700

    Implement modal

--- docker_backend/model_backend/model/static/assets/js/components/main_page/main_page.jsx
+++ docker_backend/model_backend/model/static/assets/js/components/main_page/main_page.jsx
@@ -5,10 +5,37 @@ import SearchContainer from '../search/search_container';
 import Animation from '../animation/animation';
 import ResultsContainer from '../results/results_container';
 import About from '../about/about';
+import Modal from 'react-modal';
+
+const aboutStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class MainPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {modalIsOpen: false};
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false})
   }
 
   render() {
@@ -40,10 +67,19 @@ class MainPage extends React.Component {
         </div>
 
         <div className='footer-wrapper'>
-          <div className='bottom-tab'>
-            <About />
+          <div onClick={this.openModal} className='bottom-tab'>
+            <h2>About</h2>
           </div>
         </div>
+        <Modal
+              isOpen={this.state.modalIsOpen}
+              onRequestClose={this.closeModal}
+              contentLabel='About Modal'
+              style={aboutStyles}
+              >
+              <About closeModal={this.closeModal} />
+
+            </Modal>
       </div>
     )
   }
